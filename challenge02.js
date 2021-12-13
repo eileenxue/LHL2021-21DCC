76f6ca33f6e169830f6a8f100e0050df325dcfb6
Eileen <xue.eileen@gmail.com>
2021-12-13 00:29:01 -0500

    Add challenge 2 answer

---
+++ challenge02.js
@@ -0,0 +1,49 @@
+/*****************************************
+
+Challenge #2 - One Small Step At A Time
+
+INSTRUCTIONS
+Registering new astronauts was handled manually in previous missions but it's taking too much time. 
+Knowing that the astronaut information is already listed in a data set, you propose to optimize that job with programming.
+
+Your task is to automate that process by creating a function that will take in an 
+astronaut object as a parameter and return a string using the properties of the astronaut.
+
+EXAMPLES 
+Input:
+  const exampleAstronaut = {
+      firstName: "Yuri",
+      lastName: "Gagarin",
+      nickname: "First!",
+      prefix: "Cosmonaut"
+  }
+Output: 
+  Cosmonaut: Yuri "First!" Gagarin
+
+Input: 
+  const exampleAstronaut = {
+      firstName: "Neil",
+      lastName: "Armstrong",
+      nickname: "Steps",
+      prefix: "Astronaut"
+  }
+Output: 
+  Astronaut: Neil "Steps" Armstrong
+
+*****************************************/
+
+const exampleAstronaut = {
+  firstName: "Neil",
+  lastName: "Armstrong",
+  nickname: "Steps",
+  prefix: "Astronaut"
+}
+
+const generateAstronautTag = (astronaut) => {
+  // Use template literals
+  const message = `${astronaut.prefix}: ${astronaut.firstName} "${astronaut.nickname}" ${astronaut.lastName}`
+  return message;
+}
+
+// Test the function
+console.log(generateAstronautTag(exampleAstronaut));
